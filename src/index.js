2e796dbca56a7f0d5f1f067d33a6b31740fecc9f
Chitransh Jain <cjsmcs2810@gmail.com>
2021-06-09 16:16:59 +0530

    auth ui started

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAe2U4tXT2RPhl0f56WqzMrOysDYimq410",
+  authDomain: "fir-react-ecommerce.firebaseapp.com",
+  projectId: "fir-react-ecommerce",
+  storageBucket: "fir-react-ecommerce.appspot.com",
+  messagingSenderId: "65708148259",
+  appId: "1:65708148259:web:90cec9fefcce0c34c31396",
+  measurementId: "G-E2BXHDHJL2"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
